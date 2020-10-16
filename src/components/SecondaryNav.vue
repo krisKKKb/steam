f8eb408f00ac02dcba2de16c4162b2008584af77
root <root@K2-08.ametikool.local>
2020-10-16 08:59:34 +0300

    ara kopeeri gay

--- src/components/SecondaryNav.vue
+++ src/components/SecondaryNav.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="bg-blue-steam flex mx-40 text-white">
-    <div class="p-4 border-solid border-r-2 blue-border">
-      <span class="text-white p-4 hover:text-gray-500">Sinu Pood</span>
+    <div class="p-4 border-solid border-r-2 border-gray-700 hover:bg-white">
+      <span class="text-white p-4 hover:text-gray-700">Sinu Pood</span>
     </div>
-    <div class="p-4 border-solid border-r-2 blue-border">
-      <span class="text-white p-4 hover:text-gray-500">Sirvi</span>
+    <div class="p-4 border-solid border-r-2 border-gray-700 hover:bg-white">
+      <span class="text-white p-4 hover:text-gray-700">Sirvi</span>
     </div>
-    <div class="p-4 border-solid border-r-2 blue-border">
-      <span class="text-white hover:text-gray-500 p-4">Punktide Pood</span>
+    <div class="p-4 border-solid border-r-2 border-gray-700 hover:bg-white">
+      <span class="text-white hover:text-gray-700 p-4">Punktide Pood</span>
     </div>
-    <div class="p-4 border-solid border-r-2 blue-border">
-      <span class="text-white hover:text-gray-500 p-4">Uudised</span>
+    <div class="p-4 border-solid border-r-2 border-gray-700 hover:bg-white">
+      <span class="text-white hover:text-gray-700 p-4">Uudised</span>
     </div>
-    <div class="p-4 border-solid border-r-2 blue-border">
-      <span class="text-white hover:text-gray-500 p-4">Labor</span>
+    <div class="p-4 border-solid border-r-2 border-gray-700 hover:bg-white">
+      <span class="text-white hover:text-gray-700 p-4">Labor</span>
     </div>
     <div class="p-4 ml-auto">
       <input type="text" name="search" />
